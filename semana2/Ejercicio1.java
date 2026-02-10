3d4c8a3ce113f49e836e0dc643d73768748a03ff
andresgonzalezgalindo2006-a11y <andresgonzalezgalindo.2006@gmail.com>
2026-02-10 17:05:33 +0000

    Se agrego sumar numeros y mostrar

--- semana2/Ejercicio1.java
+++ semana2/Ejercicio1.java
@@ -9,6 +9,27 @@ public class Ejercicio1 {
         for (int i = 0; i < a.length; i++) {
             System.out.println("a[" + i + ")= " + a[i]);
 
+           // sumar los numeros pares e impares y mostrar el resultado
+            int sumaPar = 0;
+            int sumaImpares = 0;
+            for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                sumaPar += a[i];
+                
+            } else {
+                sumaImpares += a[i];  
+            }
+
+            System.out.println("la suma de los pares es " + sumaPar);
+            System.out.println("la suma de los impares es " + sumaImpares);
+
+        
+
+
+          
+                
+            }
+            
         }
 
     }
