a0d96e2862b277500d3af40a5f83ca9753db8e40
andresgonzalezgalindo2006-a11y <andresgonzalezgalindo.2006@gmail.com>
2026-02-17 15:45:46 +0000

    Se agrego los atributos y el metodo constructor

---
+++ Arreglos de objetos/Estudiante.java
@@ -0,0 +1,22 @@
+public class Estudiante {
+
+private String nombre;
+private int semestre;
+private String programa;
+private double notaPromedio;
+private int edad;
+
+
+
+public Estudiante ( String nombre, int semestre, String programa, double notaPromedio, int edad){
+
+
+    this.nombre= nombre;
+    this.semestre = semestre;
+    this.programa = programa;
+    this.notaPromedio = notaPromedio;
+    this.edad = edad;
+}
+
+
+}
