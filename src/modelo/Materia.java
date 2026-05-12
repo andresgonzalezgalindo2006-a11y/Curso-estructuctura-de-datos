44564a358bf4e49c415fd21ed56568ab6b3515ce
andresgonzalezgalindo2006-a11y <andresgonzalezgalindo.2006@gmail.com>
2026-05-12 16:54:31 +0000

    Implemente clase materia

---
+++ src/modelo/Materia.java
@@ -0,0 +1,30 @@
+package src.modelo;
+
+public class Materia {
+    private String nombre;
+    private int codigo;
+
+    public Materia(String nombre, int codigo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+
+    
+}
