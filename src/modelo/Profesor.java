fcb28da2caca551e8d90eefc2f384bd572421633
andresgonzalezgalindo2006-a11y <andresgonzalezgalindo.2006@gmail.com>
2026-05-14 23:19:28 +0000

    implemente clase profesor

---
+++ src/modelo/Profesor.java
@@ -0,0 +1,26 @@
+package modelo;
+
+public class Profesor extends Persona {
+
+    private String departamento;
+
+    private double salario;
+
+    public Profesor(
+          
+            String departamento,
+            double salario) {
+
+        super(nombre, id, email);
+
+        this.departamento = departamento;
+        this.salario = salario;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+
+        System.out.println("Profesor: " + getNombre());
+        System.out.println("Departamento: " + departamento);
+    }
+}
